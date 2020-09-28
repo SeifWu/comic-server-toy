0de71ef0a6e7ce22c385dd8f71b642962e1007fe
Seif Wu <seif.wuxf@gmail.com>
2020-09-28 13:27:27 +0800

    feat: 添加爬取详情页接口

--- app/crawl/qiman6/comic_page.go
+++ app/crawl/qiman6/comic_page.go
@@ -1,41 +1,83 @@
 package qiman6
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/extensions"
 )
 
 // ComicPage 漫画详情页
-func (q *New) ComicPage(url string) {
-	c := colly.NewCollector(
-		colly.Async(true),
-		colly.UserAgent("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"),
-	)
+func (q *New) ComicPage(id string) (Comic, error) {
 
-	// c.Limit(&colly.LimitRule{DomainGlob: "*.douban.*", Parallelism: 5})
+	var moreChapter = map[string]string{"id": id, "id2": "1"}
+	var comic Comic
+	var chapterList []Chapter
+	var otherChapters []OtherChapter
+	c := colly.NewCollector()
+	visited := false
 
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("Visiting", r.URL)
-	})
+	extensions.RandomUserAgent(c)
+	extensions.Referer(c)
 
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-
+	// count links
 	c.OnHTML(".mainForm", func(e *colly.HTMLElement) {
-		cover := e.ChildAttr(".comicInfo > .ib.cover > .img > img", "src")
-		fmt.Println("封面：", cover)
-		lastChapter := e.ChildText(".comicInfo > .ib.cover > .img > a")
-		fmt.Println("最新话：", lastChapter)
-		title := e.ChildText(".comicInfo > .ib.info > .name_mh")
-		fmt.Println("漫画名：", title)
-		author := e.ChildText(".comicInfo > .ib.info > p:not(.gray) > .ib.l")
-		fmt.Println("作者: ", author)
-		introduce := e.ChildText(".comicInfo > .ib.info > .content")
-		fmt.Println("简介: ", introduce)
+		title := e.ChildText(".comicInfo .ib.info h1.name_mh")
+		log.Println("获取名称: ", title)
+
+		introduction := e.ChildText(".comicInfo .ib.info p.content")
+		log.Println("获取简介: ", introduction)
+
+		cover := e.ChildAttr(".comicInfo .ib.cover .img img", "src")
+		log.Println("获取封面: ", cover)
+
+		latestChapter := e.ChildText(".comicInfo .ib.cover .img a.op.ib")
+		log.Println("获取最新章节: ", latestChapter)
+
+		e.ForEach(".chapterList#chapterList .list#chapter-list1 a.ib", func(i int, h *colly.HTMLElement) {
+			chapter := Chapter{
+				Title: h.Text,
+				URL:   Domain + h.Attr("href"),
+			}
+			chapterList = append(chapterList, chapter)
+		})
+
+		comic.Title = title
+		comic.Introduction = introduction
+		comic.Cover = cover
+		comic.LatestChapter = latestChapter
+		comic.ChapterList = chapterList
 	})
 
-	c.Visit("http://" + url)
+	c.OnResponse(func(r *colly.Response) {
+		err := json.Unmarshal(r.Body, &otherChapters)
+		if err != nil {
+			log.Println("解析JSON错误: ", err.Error())
+		}
+		if !visited {
+			visited = true
+			r.Request.Post(fmt.Sprintf("http://%s/bookchapter/", Domain), moreChapter)
+		}
+	})
+
+	c.Visit(fmt.Sprintf("http://%s/%s/", Domain, id))
+	chapters := handleOtherChapters(otherChapters, id)
+	comic.ChapterList = append(comic.ChapterList, chapters...)
+	return comic, nil
+}
+
+func handleOtherChapters(otherChapters []OtherChapter, id string) []Chapter {
+	var chapters []Chapter
+	for _, value := range otherChapters {
+		url := fmt.Sprintf("%s/%s/", Domain, id)
+		chapter := Chapter{
+			URL:   url + value.ChapterID + ".html",
+			Title: value.ChapterName,
+		}
+		chapters = append(chapters, chapter)
+	}
+
+	return chapters
 }
--- app/crawl/qiman6/qiman6.go
+++ app/crawl/qiman6/qiman6.go
@@ -2,3 +2,27 @@ package qiman6
 
 // New 爬取 http://www.qiman6.com 内容
 type New struct{}
+
+// Domain 目标站点
+var Domain = "www.qiman6.com"
+
+// OtherChapter 请求来的章节
+type OtherChapter struct {
+	ChapterID   string `json:"chapterid"`
+	ChapterName string `json:"chaptername"`
+}
+
+// Chapter 章节
+type Chapter struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
+
+// Comic 漫画结构体
+type Comic struct {
+	Title         string    `json:"title"`
+	Introduction  string    `json:"introduction"`
+	Cover         string    `json:"cover"`
+	LatestChapter string    `json:"latestChapter"`
+	ChapterList   []Chapter `json:"chapterList"`
+}
