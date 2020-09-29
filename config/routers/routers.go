5669cf7e494a5ee436f596e51ad8add31ed74139
Seif Wu <seif.wuxf@gmail.com>
2020-09-29 11:00:47 +0800

    chore: 移除 session 相关代码

--- config/routers/routers.go
+++ config/routers/routers.go
@@ -6,23 +6,12 @@ import (
 
 	"seifwu/app/middleware"
 
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 // Routers 路由
 func Routers() *gin.Engine {
 	router := gin.Default()
-	sessionSecretKey := viper.GetString("session.secretKey")
-
-	// store := cookie.NewStore([]byte(""))
-	// router.Use(sessions.Sessions("appSession", store))
-
-	store, _ := redis.NewStoreWithDB(10, "tcp", "localhost:6379", "", "1", []byte(sessionSecretKey))
-	store.Options(sessions.Options{Path: "/", MaxAge: 2629746, HttpOnly: true})
-	router.Use(sessions.Sessions("appSession", store))
 
 	v1 := router.Group("/api/v1")
 	{
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/antchfx/htmlquery v1.2.3 // indirect
 	github.com/antchfx/xmlquery v1.3.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
